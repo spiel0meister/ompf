ab3ca2ad07789e8c6634b3320b83d3e650b7b471
spiel0meister <soviczan7@gmail.com>
2025-04-30 16:40:12 +0200

    fixed stuff with version

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -ex
 
 version=$(git describe --always HEAD)
 
-odin build . -debug -extra-linker-flags:-L$HOME/.local/lib/ "-define:VERSION=$version"
+odin build . -debug -extra-linker-flags:-L$HOME/.local/lib/ "-define:VERSION=\"$version\""
 
